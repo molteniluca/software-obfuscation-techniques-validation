7cdb8f934f1b8ef2f3ee5bb6a2026376e6b39371
XenoVincent <71254499+XenoVincent@users.noreply.github.com>
2021-08-12 16:52:24 +0200

    minor fixup import parameter written wrong add a comment line

--- sotv/Tracer/instruction.py
+++ sotv/Tracer/instruction.py
@@ -1,5 +1,5 @@
 from sotv.Tracer.structures import opcodes, registers
-from instruction_adapter import AdapterInterface
+from sotv.Tracer.instruction_adapter import AdapterInterface
 
 
 class Instruction:
@@ -11,13 +11,14 @@ class Instruction:
     r2 -- second register
     r3 -- third register
     immediate -- immediate if needed
+    ins_adapter -- strategy to apply for this type of instruction
     """
     opcode: opcodes
     r1: registers
     r2: registers
     r3: registers
     immediate: int
-    ins_adapter: None
+    ins_adapter: AdapterInterface
 
     def __init__(self, opcode, r1, r2, r3, immediate):
         self.opcode = opcode
