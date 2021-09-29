740a723c93ab1cccfb75b67a678738b720f3f5b2
XenoVincent <71254499+XenoVincent@users.noreply.github.com>
2021-09-29 10:19:04 +0200

    add trash metric and variables metric

--- sotv/scoreCalculator/score.py
+++ sotv/scoreCalculator/score.py
@@ -2,12 +2,27 @@ from sotv.Tracer import tracer
 
 
 class Metrics:
-    metrics = {"higher_register_heat": 0, "average_register_heat": 0,
-               "variable_stay_in_a_register_after_been_used(move or sw)": False}
-    tracer: None
+    metrics_heat: dict
+    metrics_trash: dict
+    tracer: tracer.Tracer
 
     def __init__(self, completed_tracer):
         self.tracer: completed_tracer
 
     def metric_score(self):
-        pass
+        for instruction in self.tracer.execution_dump.dump:
+            registers_status = self.tracer.tracing_graph[instruction]
+            if registers_status is not None:
+                temp_reg = instruction.executed_instruction.modified_register()
+                for reg in registers_status.keys():
+                    self.metrics_heat[reg] += 1
+                for reg in self.metrics_trash.keys():
+                    self.metrics_trash[reg] += 1
+                if temp_reg is not None:
+                    if temp_reg not in registers_status.keys():
+                        self.metrics_trash[temp_reg] += 1
+        self.print()
+
+    def print(self):
+        print(self.metrics_trash)
+        print(self.metrics_heat)
