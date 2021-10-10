c3ddbe61a4c5f862e78aab2136ad3ba4c1fb2324
XenoVincent <71254499+XenoVincent@users.noreply.github.com>
2021-10-10 16:32:38 +0200

    update score.py with relative comment

--- sotv/scoreCalculator/score.py
+++ sotv/scoreCalculator/score.py
@@ -3,6 +3,42 @@ from typing import Dict
 from sotv.Tracer.structures import Register
 
 
+# Updates the dictionary of the variable at his last edit
+def lock(variables, string):
+    temp_list = string.split("_")
+    if temp_list[0] in variables.keys():
+        if temp_list[1] > variables[temp_list[0]]:
+            variables[temp_list[0]] = temp_list[1]
+    else:
+        variables[temp_list[0]] = temp_list[1]
+
+
+# Condition that check if there are in the same register a variable not updated and ignore duplicates
+def condition(register_status, register, lock_dict):
+    variable_dict = {}
+    # initialize variable_dict with the variable that are in the register
+    for variable in register_status[register]:
+        temp = variable.split("_")
+        if temp[0] in variable_dict.keys():
+            if variable_dict[temp[0]] < temp[1]:
+                variable_dict[temp[0]] = temp[1]
+        else:
+            variable_dict[temp[0]] = temp[1]
+    # search if in another register there is a more update version of the variables that are in variable_dict
+    for reg in register_status.keys():
+        if reg != register:
+            for elem in register_status[reg]:
+                temp = elem.split("_")
+                if temp[0] in variable_dict.keys():
+                    if temp[1] > variable_dict[temp[0]]:
+                        return True
+    # in the case of register a0:{a_0, a_1} but the last edit on the a is a_3
+    for var in variable_dict.keys():
+        if var in lock_dict.keys() and lock_dict[var] > variable_dict[var]:
+            return True  # this return is reached in some wrong cases
+    return False
+
+
 class Metrics:
     metrics_heat: Dict[Register, int]
     metrics_trash: Dict[Register, int]
@@ -14,43 +50,39 @@ class Metrics:
         self.metrics_trash = {}
 
     def metric_score(self):
-        registers_lock_list = []
+        lock_dict = {}
         for instruction in self.tracer.execution_dump.dump:
             try:
                 registers_status = self.tracer.tracing_graph[instruction]
-            except:
+            except KeyError:
                 registers_status = None
             if registers_status is not None:
-                self.lock(registers_lock_list, instruction.executed_instruction)
-                temp_reg = instruction.executed_instruction.modified_register()
                 for reg in registers_status.keys():
-                    if reg not in registers_lock_list:
+                    for string in registers_status[reg]:
+                        lock(lock_dict, string)
+                    for string in registers_status[reg]:
+                        temp_split = string.split("_")
                         if reg in self.metrics_heat.keys():
-                            self.metrics_heat[reg] += 1
+                            if lock_dict[temp_split[0]] <= temp_split[1]:
+                                self.metrics_heat[reg] += 1
+                            else:
+                                if condition(registers_status, reg, lock_dict):
+                                    if reg in self.metrics_trash.keys():
+                                        self.metrics_trash[reg] += 1
+                                    else:
+                                        self.metrics_trash[reg] = 1
                         else:
                             self.metrics_heat[reg] = 1
-                    else:
-                        if reg in self.metrics_trash.keys():
-                            self.metrics_trash[reg] += 1
-                        else:
-                            self.metrics_trash[reg] = 1
-                for reg in self.metrics_trash.keys():
-                    if reg in self.metrics_trash.keys():
-                        self.metrics_trash[reg] += 1
-                if temp_reg is not None:
-                    if temp_reg not in registers_status.keys():
-                        if temp_reg in self.metrics_trash.keys():
-                            self.metrics_trash[temp_reg] += 1
-                        else:
-                            self.metrics_trash[temp_reg] = 1
+                    temp_reg = instruction.executed_instruction.modified_register()
+                    # case that the instruction modified a register that is not in the tracer
+                    if temp_reg is not None:
+                        if temp_reg not in registers_status.keys():
+                            if temp_reg in self.metrics_trash.keys():
+                                self.metrics_trash[temp_reg] += 1
+                            else:
+                                self.metrics_trash[temp_reg] = 1
         self.print()
 
-    def lock(self, registers, instruction):
-        if instruction.opcode == "mv":
-            if instruction.modified_register() in registers:
-                registers.remove(instruction.modified_register())
-            registers.append(instruction.r2)
-
     def print(self):
-        print(self.metrics_trash)
         print(self.metrics_heat)
+        print(self.metrics_trash)
